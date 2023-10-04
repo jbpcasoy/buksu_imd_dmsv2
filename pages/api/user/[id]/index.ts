0c78eb604d9cb838cead90e710c4cead29c2733f
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-10-04 21:32:39 +0800

    implemented get users and get user

---
+++ pages/api/user/[id]/index.ts
@@ -0,0 +1,45 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as Yup from "yup";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const getHandler = async () => {
+    const validator = Yup.object({
+      id: Yup.string().required(),
+    });
+    const query = validator.cast(req.query);
+    try {
+      await validator.validate(query);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+
+    const prisma = new PrismaClient();
+    const { id } = query;
+    try {
+      const user = await prisma.user.findFirstOrThrow({
+        where: {
+          id: {
+            equals: id,
+          },
+        },
+      });
+
+      await prisma.$disconnect();
+      return res.json(user);
+    } catch (error) {
+      await prisma.$disconnect();
+      return res.status(400).json(error);
+    }
+  };
+
+  switch (req.method) {
+    case "GET":
+      return await getHandler();
+  }
+  res.status(200).json({ name: "John Doe" });
+}
---
+++ pages/api/user/index.ts
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as Yup from "yup";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const getHandler = async () => {
+    const validator = Yup.object({
+      skip: Yup.number().required(),
+      take: Yup.number().required(),
+    });
+    const query = validator.cast(req.query);
+    try {
+      await validator.validate(query);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+
+    const prisma = new PrismaClient();
+    const { skip, take } = query;
+    try {
+      const users = await prisma.user.findMany({
+        skip,
+        take,
+      });
+      const count = await prisma.user.count()
+
+      await prisma.$disconnect();
+      return res.json({users, count});
+    } catch (error) {
+      await prisma.$disconnect();
+      return res.status(400).json(error);
+    }
+  };
+
+  switch (req.method) {
+    case "GET":
+      return await getHandler();
+  }
+  res.status(200).json({ name: "John Doe" });
+}
