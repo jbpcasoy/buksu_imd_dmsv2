0fe06578f81c3c63d050eff064538d3d33ecc566
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-10-04 22:15:06 +0800

    implemented get all faculty

--- pages/api/faculty/index.ts
+++ pages/api/faculty/index.ts
@@ -37,9 +37,40 @@ export default async function handler(
     }
   };
 
+  const getHandler = async () => {
+    const validator = Yup.object({
+      take: Yup.number().required(),
+      skip: Yup.number().required(),
+    });
+
+    try {
+      await validator.validate(req.query);
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+
+    const prisma = new PrismaClient();
+    const { skip, take } = validator.cast(req.query);
+    try {
+      const faculties = await prisma.faculty.findMany({
+        skip,
+        take,
+      });
+      const count = await prisma.faculty.count();
+
+      await prisma.$disconnect();
+      return res.json({ faculties, count });
+    } catch (error) {
+      await prisma.$disconnect();
+      return res.status(400).json({ error });
+    }
+  };
+
   switch (req.method) {
     case "POST":
       return await postHandler();
+    case "GET":
+      return await getHandler();
     default:
       return res.status(405).send(`${req.method} Not Allowed`);
   }
