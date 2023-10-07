0fd0d9095535a00d6c614a00ac84b62d51b5e22d
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-10-07 11:13:21 +0800

    fixed department ability calls

--- pages/api/department/[id]/index.ts
+++ pages/api/department/[id]/index.ts
@@ -20,7 +20,7 @@ export default async function handler(
     return res.status(401).json({ error: { message: "Unauthorized" } });
   }
 
-  const ability = departmentAbility(user);
+  const ability = departmentAbility({user});
 
   const getHandler = async () => {
     const validator = Yup.object({
