93c63c158ba820590d145b78342e39169d836858
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-11-30 20:19:38 +0800

    implemented PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,36 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head title='BukSU IMD DMS'>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png?v=2'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png?v=2'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png?v=2'
+        />
+        <link rel='manifest' href='/manifest.json?v=2' />
+        <link
+          rel='mask-icon'
+          href='/safari-pinned-tab.svg?v=2'
+          color='#152033'
+        />
+        <link rel='shortcut icon' href='/favicon.ico?v=2' />
+        <meta name='apple-mobile-web-app-title' content='BukSU IMD DMS' />
+        <meta name='application-name' content='BukSU IMD DMS' />
+        <meta name='msapplication-TileColor' content='#f2c050' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
       <body>
         <Main />
         <NextScript />
