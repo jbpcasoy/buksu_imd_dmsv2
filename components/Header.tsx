51f8225605cef84602c2b6904d289fc9aeb60551
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-12-11 16:54:55 +0800

    implemented notification count indicator

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import useActiveChairpersonMe from "@/hooks/useActiveChairpersonMe";
 import useActiveCoordinatorMe from "@/hooks/useActiveCoordinatorMe";
 import useActiveDeanMe from "@/hooks/useActiveDeanMe";
 import useActiveIDDCoordinatorMe from "@/hooks/useActiveIDDCoordinatorMe";
+import useEventsMe from "@/hooks/useEventsMe";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -20,6 +21,10 @@ export default function Header({ onToggleSidebar }: HeaderProps) {
   const [state, setState] = useState({
     initialized: false,
     openSidebar: true,
+  });
+  const { count } = useEventsMe({
+    skip: 0,
+    take: 0,
   });
 
   useEffect(() => {
@@ -95,16 +100,33 @@ export default function Header({ onToggleSidebar }: HeaderProps) {
         <div className='flex justify-center items-center space-x-2'>
           <Link
             href='/notification'
-            className='fill-palette_white hover:fill-palette_orange'
+            className='fill-palette_white group'
           >
-            {/* <span className='font-normal '>{eventCount.count}</span> Notification */}
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              height='1em'
-              viewBox='0 0 448 512'
-            >
-              <path d='M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z' />
-            </svg>
+            <div className='relative'>
+              {count < 1 && (
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  height='1em'
+                  viewBox='0 0 448 512'
+                  className="group-hover:fill-palette_orange"
+                >
+                  <path d='M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z' />
+                </svg>
+              )}
+              {count > 0 && (
+                <div className='inline-flex items-center justify-center px-1 bg-palette_orange rounded-full text-xs font-semibold text-palette_blue p-1 space-x-1'>
+                  <span>{count <= 99 ? count : "99+"}</span>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    height='1em'
+                    viewBox='0 0 448 512'
+                    className='w-4 h-4 group-hover:fill-palette_blue'
+                  >
+                    <path d='M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z' />
+                  </svg>
+                </div>
+              )}
+            </div>
           </Link>
           <Link href='/profile' className=''>
             <img
