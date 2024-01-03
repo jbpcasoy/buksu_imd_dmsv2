8f07a14cc84588d39516d883fde583e1b35b7717
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-01-03 15:33:25 +0800

    implemented rbac in active dean

--- pages/api/active_dean/me/index.ts
+++ pages/api/active_dean/me/index.ts
@@ -1,12 +1,9 @@
 import prisma from "@/prisma/client";
-import activeDeanAbility from "@/services/ability/activeDeanAbility";
 import getServerUser from "@/services/getServerUser";
 import logger from "@/services/logger";
 
-import { accessibleBy } from "@casl/prisma";
 import { User } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-import * as Yup from "yup";
 
 export default async function handler(
   req: NextApiRequest,
@@ -20,28 +17,23 @@ export default async function handler(
     return res.status(401).json({ error: { message: "Unauthorized" } });
   }
 
-  let ability = activeDeanAbility({ user });
-
   const getHandler = async () => {
     try {
-      const activeDean = await prisma.activeDean.findFirstOrThrow(
-        {
-          where: {
-            AND: [
-              accessibleBy(ability).ActiveDean,
-              {
-                Dean: {
-                  Faculty: {
-                    userId: {
-                      equals: user.id,
-                    },
+      const activeDean = await prisma.activeDean.findFirstOrThrow({
+        where: {
+          AND: [
+            {
+              Dean: {
+                Faculty: {
+                  userId: {
+                    equals: user.id,
                   },
                 },
               },
-            ],
-          },
-        }
-      );
+            },
+          ],
+        },
+      });
 
       return res.json(activeDean);
     } catch (error: any) {
