5559316251d949a2eb29222a251c876831643be9
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-01-10 13:10:15 +0800

    implemented rbac on idd coordinator suggestion action taken

--- pages/api/idd_coordinator_suggestion_item_action_taken/[id]/index.ts
+++ pages/api/idd_coordinator_suggestion_item_action_taken/[id]/index.ts
@@ -1,9 +1,6 @@
 import prisma from "@/prisma/client";
-import iDDCoordinatorSuggestionItemActionTakenAbility from "@/services/ability/iDDCoordinatorSuggestionItemActionTakenAbility";
 import getServerUser from "@/services/getServerUser";
 import logger from "@/services/logger";
-import { ForbiddenError } from "@casl/ability";
-import { accessibleBy } from "@casl/prisma";
 import { User } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as Yup from "yup";
@@ -20,7 +17,6 @@ export default async function handler(
     logger.error(error);
     return res.status(401).json({ error: { message: "Unauthorized" } });
   }
-  const ability = iDDCoordinatorSuggestionItemActionTakenAbility({ user });
 
   const getHandler = async () => {
     try {
@@ -35,7 +31,6 @@ export default async function handler(
         await prisma.iDDCoordinatorSuggestionItemActionTaken.findFirstOrThrow({
           where: {
             AND: [
-              accessibleBy(ability).IDDCoordinatorSuggestionItemActionTaken,
               {
                 id: {
                   equals: id,
@@ -62,25 +57,41 @@ export default async function handler(
 
       await validator.validate(req.query);
 
-      ForbiddenError.from(ability).throwUnlessCan(
-        "delete",
-        "IDDCoordinatorSuggestionItemActionTaken"
-      );
-
       const { id } = validator.cast(req.query);
 
-      const cITLRevision = await prisma.cITLRevision.findFirst({
-        where: {
-          IMFile: {
-            DepartmentRevision: {
-              CoordinatorEndorsement: {
-                DeanEndorsement: {
-                  IDDCoordinatorSuggestion: {
-                    IDDCoordinatorSuggestionItem: {
-                      some: {
-                        IDDCoordinatorSuggestionItemActionTaken: {
-                          id: {
-                            equals: id,
+      if (!user.isAdmin) {
+        const iM = await prisma.iM.findFirstOrThrow({
+          where: {
+            IMFile: {
+              some: {
+                DepartmentReview: {
+                  CoordinatorReview: {
+                    CoordinatorSuggestion: {
+                      SubmittedCoordinatorSuggestion: {
+                        DepartmentReviewed: {
+                          DepartmentRevision: {
+                            some: {
+                              CoordinatorEndorsement: {
+                                DeanEndorsement: {
+                                  IDDCoordinatorSuggestion: {
+                                    SubmittedIDDCoordinatorSuggestion: {
+                                      IDDCoordinatorSuggestion: {
+                                        IDDCoordinatorSuggestionItem: {
+                                          some: {
+                                            IDDCoordinatorSuggestionItemActionTaken:
+                                              {
+                                                id: {
+                                                  equals: id,
+                                                },
+                                              },
+                                          },
+                                        },
+                                      },
+                                    },
+                                  },
+                                },
+                              },
+                            },
                           },
                         },
                       },
@@ -90,24 +101,78 @@ export default async function handler(
               },
             },
           },
-          OR: [
-            {
-              ReturnedCITLRevision: {
-                is: null,
-              },
-            },
-            {
-              ReturnedCITLRevision: {
-                SubmittedReturnedCITLRevision: {
-                  is: null,
+        });
+        const faculty = await prisma.faculty.findFirst({
+          where: {
+            ActiveFaculty: {
+              Faculty: {
+                User: {
+                  id: {
+                    equals: user.id,
+                  },
                 },
               },
             },
-          ],
-        },
-      });
-      if (cITLRevision) {
-        throw new Error("IM already revised.");
+          },
+        });
+        if (!faculty) {
+          return res.status(403).json({
+            error: {
+              message:
+                "Only an active faculty is allowed to perform this action",
+            },
+          });
+        }
+
+        if (faculty.id !== iM.facultyId) {
+          return res.status(403).json({
+            error: {
+              message:
+                "You are not allowed to delete this idd coordinator suggestion item action taken",
+            },
+          });
+        }
+
+        const cITLRevision = await prisma.cITLRevision.findFirst({
+          where: {
+            IMFile: {
+              DepartmentRevision: {
+                CoordinatorEndorsement: {
+                  DeanEndorsement: {
+                    IDDCoordinatorSuggestion: {
+                      IDDCoordinatorSuggestionItem: {
+                        some: {
+                          IDDCoordinatorSuggestionItemActionTaken: {
+                            id: {
+                              equals: id as string,
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            OR: [
+              {
+                ReturnedCITLRevision: {
+                  is: null,
+                },
+              },
+              {
+                ReturnedCITLRevision: {
+                  SubmittedReturnedCITLRevision: {
+                    is: null,
+                  },
+                },
+              },
+            ],
+          },
+        });
+        if (cITLRevision) {
+          throw new Error("IM already revised.");
+        }
       }
 
       const iDDCoordinatorSuggestionItemActionTaken =
@@ -134,13 +199,124 @@ export default async function handler(
 
       await validator.validate(req.body);
 
-      ForbiddenError.from(ability).throwUnlessCan(
-        "update",
-        "IDDCoordinatorSuggestionItemActionTaken"
-      );
-
       const { id } = req.query;
       const { value } = validator.cast(req.body);
+
+      if (!user.isAdmin) {
+        const iM = await prisma.iM.findFirstOrThrow({
+          where: {
+            IMFile: {
+              some: {
+                DepartmentReview: {
+                  CoordinatorReview: {
+                    CoordinatorSuggestion: {
+                      SubmittedCoordinatorSuggestion: {
+                        DepartmentReviewed: {
+                          DepartmentRevision: {
+                            some: {
+                              CoordinatorEndorsement: {
+                                DeanEndorsement: {
+                                  IDDCoordinatorSuggestion: {
+                                    SubmittedIDDCoordinatorSuggestion: {
+                                      IDDCoordinatorSuggestion: {
+                                        IDDCoordinatorSuggestionItem: {
+                                          some: {
+                                            IDDCoordinatorSuggestionItemActionTaken:
+                                              {
+                                                id: {
+                                                  equals: id as string,
+                                                },
+                                              },
+                                          },
+                                        },
+                                      },
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        });
+        const faculty = await prisma.faculty.findFirst({
+          where: {
+            ActiveFaculty: {
+              Faculty: {
+                User: {
+                  id: {
+                    equals: user.id,
+                  },
+                },
+              },
+            },
+          },
+        });
+        if (!faculty) {
+          return res.status(403).json({
+            error: {
+              message:
+                "Only an active faculty is allowed to perform this action",
+            },
+          });
+        }
+
+        if (faculty.id !== iM.facultyId) {
+          return res.status(403).json({
+            error: {
+              message:
+                "You are not allowed to update this idd coordinator suggestion item action taken",
+            },
+          });
+        }
+
+        const cITLRevision = await prisma.cITLRevision.findFirst({
+          where: {
+            IMFile: {
+              DepartmentRevision: {
+                CoordinatorEndorsement: {
+                  DeanEndorsement: {
+                    IDDCoordinatorSuggestion: {
+                      IDDCoordinatorSuggestionItem: {
+                        some: {
+                          IDDCoordinatorSuggestionItemActionTaken: {
+                            id: {
+                              equals: id as string,
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            OR: [
+              {
+                ReturnedCITLRevision: {
+                  is: null,
+                },
+              },
+              {
+                ReturnedCITLRevision: {
+                  SubmittedReturnedCITLRevision: {
+                    is: null,
+                  },
+                },
+              },
+            ],
+          },
+        });
+        if (cITLRevision) {
+          throw new Error("IM already revised.");
+        }
+      }
 
       const iDDCoordinatorSuggestionItemActionTaken =
         await prisma.iDDCoordinatorSuggestionItemActionTaken.update({
