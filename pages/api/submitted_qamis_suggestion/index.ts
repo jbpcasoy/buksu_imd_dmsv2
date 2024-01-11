6dc1b3ec6514ae7e802dffb86ca6f5c3235fa5d6
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-01-11 14:41:40 +0800

    implemented rbac on submitted qamis suggestion

--- pages/api/submitted_qamis_suggestion/index.ts
+++ pages/api/submitted_qamis_suggestion/index.ts
@@ -1,9 +1,6 @@
 import prisma from "@/prisma/client";
-import submittedQAMISSuggestionAbility from "@/services/ability/submittedQAMISSuggestionAbility";
 import getServerUser from "@/services/getServerUser";
 import logger from "@/services/logger";
-import { ForbiddenError } from "@casl/ability";
-import { accessibleBy } from "@casl/prisma";
 import { User } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as Yup from "yup";
@@ -20,7 +17,6 @@ export default async function handler(
     logger.error(error);
     return res.status(401).json({ error: { message: "Unauthorized" } });
   }
-  const ability = submittedQAMISSuggestionAbility({ user });
 
   const postHandler = async () => {
     try {
@@ -29,12 +25,87 @@ export default async function handler(
       });
       await validator.validate(req.body);
 
-      ForbiddenError.from(ability).throwUnlessCan(
-        "create",
-        "SubmittedQAMISSuggestion"
-      );
-
       const { qAMISSuggestionId } = validator.cast(req.body);
+
+      if (!user.isAdmin) {
+        const iM = await prisma.iM.findFirstOrThrow({
+          where: {
+            IMFile: {
+              some: {
+                DepartmentReview: {
+                  CoordinatorReview: {
+                    CoordinatorSuggestion: {
+                      SubmittedCoordinatorSuggestion: {
+                        DepartmentReviewed: {
+                          DepartmentRevision: {
+                            some: {
+                              CoordinatorEndorsement: {
+                                DeanEndorsement: {
+                                  IDDCoordinatorSuggestion: {
+                                    SubmittedIDDCoordinatorSuggestion: {
+                                      CITLRevision: {
+                                        some: {
+                                          IDDCoordinatorEndorsement: {
+                                            CITLDirectorEndorsement: {
+                                              IDDCoordinatorEndorsement: {
+                                                CITLDirectorEndorsement: {
+                                                  QAMISSuggestion: {
+                                                    id: {
+                                                      equals: qAMISSuggestionId,
+                                                    },
+                                                  },
+                                                },
+                                              },
+                                            },
+                                          },
+                                        },
+                                      },
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        const faculty = await prisma.faculty.findFirst({
+          where: {
+            ActiveFaculty: {
+              Faculty: {
+                User: {
+                  id: {
+                    equals: user.id,
+                  },
+                },
+              },
+            },
+          },
+        });
+        if (!faculty) {
+          return res.status(403).json({
+            error: {
+              message:
+                "Only an active faculty is allowed to perform this action",
+            },
+          });
+        }
+
+        if (faculty.id !== iM.facultyId) {
+          return res.status(403).json({
+            error: {
+              message: "You are not allowed to submit this QAMIS suggestion",
+            },
+          });
+        }
+      }
 
       const qAMISSuggestionItemCount = await prisma.qAMISSuggestionItem.count({
         where: {
@@ -86,17 +157,13 @@ export default async function handler(
         await prisma.submittedQAMISSuggestion.findMany({
           skip,
           take,
-          where: {
-            AND: [accessibleBy(ability).SubmittedQAMISSuggestion],
-          },
+          where: {},
           orderBy: {
             updatedAt: "desc",
           },
         });
       const count = await prisma.submittedQAMISSuggestion.count({
-        where: {
-          AND: [accessibleBy(ability).SubmittedQAMISSuggestion],
-        },
+        where: {},
       });
 
       return res.json({ submittedQAMISSuggestions, count });
