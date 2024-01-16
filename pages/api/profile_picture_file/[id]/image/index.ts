47cc299d36e1097405e7448dffbd026cfad31ea1
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-01-16 17:01:49 +0800

    implemented rbac on profile picture file

--- pages/api/profile_picture_file/[id]/image/index.ts
+++ pages/api/profile_picture_file/[id]/image/index.ts
@@ -1,14 +1,10 @@
 import prisma from "@/prisma/client";
-import profilePictureFileAbility from "@/services/ability/profilePictureFileAbility";
 import getServerUser from "@/services/getServerUser";
 import logger from "@/services/logger";
-import { ForbiddenError, subject } from "@casl/ability";
-import { accessibleBy } from "@casl/prisma";
 import { User } from "@prisma/client";
 import fs from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
-import { promisify } from "util";
 
 export default async function handler(
   req: NextApiRequest,
@@ -22,7 +18,6 @@ export default async function handler(
     logger.error(error);
     return res.status(401).json({ error: { message: "Unauthorized" } });
   }
-  const ability = profilePictureFileAbility({ user });
 
   const getHandler = async () => {
     try {
@@ -31,7 +26,6 @@ export default async function handler(
         await prisma.profilePictureFile.findFirstOrThrow({
           where: {
             AND: [
-              accessibleBy(ability).ProfilePictureFile,
               {
                 id: {
                   equals: id as string,
