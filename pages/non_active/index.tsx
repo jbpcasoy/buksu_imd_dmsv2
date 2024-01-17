a66e75f391817f0b5c0a0efe60f8d7887b68e9ce
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-01-17 14:00:11 +0800

    fixed imerc citl director endorsed tab

---
+++ pages/non_active/index.tsx
@@ -0,0 +1,40 @@
+import MainLayout from "@/components/MainLayout";
+import useActiveCITLDirectorMe from "@/hooks/useActiveCITLDirectorMe";
+import useActiveFaculty from "@/hooks/useActiveFaculty";
+import useActiveFacultyMe from "@/hooks/useActiveFacultyMe";
+import useActiveIDDCoordinatorMe from "@/hooks/useActiveIDDCoordinatorMe";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function NonActive() {
+  const { data: session } = useSession({
+    required: true,
+  });
+  const router = useRouter();
+
+  const activeFaculty = useActiveFacultyMe();
+  const activeCITLDirector = useActiveCITLDirectorMe();
+  const activeIDDCoordinator = useActiveIDDCoordinatorMe();
+
+  useEffect(() => {
+    if (activeFaculty || activeCITLDirector || activeIDDCoordinator) {
+      router.replace("/");
+    }
+  }, [activeFaculty, activeCITLDirector, activeIDDCoordinator]);
+
+  return (
+    <MainLayout>
+      <div>
+        {!activeFaculty && (
+          <div>
+            <p>
+              Welcome, you are not yet assigned as a faculty. Please be patient
+              while the admin sets your roles.
+            </p>
+          </div>
+        )}
+      </div>
+    </MainLayout>
+  );
+}
