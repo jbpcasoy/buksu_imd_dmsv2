48b46dfdcc05b83df601e2bcfc5e39c8fe35ae1f
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-01-22 16:07:33 +0800

    implemented QAMIS file manager

---
+++ hooks/useFileManagerQAMISFiles.ts
@@ -0,0 +1,35 @@
+import FileMetadata from "@/constants/FileMetadata";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface UseFileManagerQAMISFilesProps {
+  skip: number;
+  take: number;
+  filter?: object;
+}
+
+export default function useFileManagerQAMISFiles({skip, take, filter}: UseFileManagerQAMISFilesProps) {
+    const [state, setState] = useState<{fileMetadatas: FileMetadata[], count: number}>({
+        count: 0,
+        fileMetadatas: []
+      });
+    
+      useEffect(() => {
+        axios
+          .get("/api/file_manager/qamis", {
+            params: {
+              skip,
+              take,
+              filter
+            },
+          })
+          .then((res) => {
+            setState(res.data);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, [skip, take, filter]);
+    
+      return state;
+}
---
+++ pages/api/file_manager/qamis/[filename]/pdf/index.ts
@@ -0,0 +1,44 @@
+import getServerUser from "@/services/getServerUser";
+import logger from "@/services/logger";
+import { User } from "@prisma/client";
+import fs from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let user: User;
+
+  try {
+    user = await getServerUser(req, res);
+  } catch (error) {
+    logger.error(error);
+    return res.status(401).json({ error: { message: "Unauthorized" } });
+  }
+
+  const getHandler = async () => {
+    try {
+      const { filename } = req.query;
+
+      res.setHeader("Content-Type", `application/pdf`);
+
+      const destination = path.join(process.cwd(), `/files/qamis/${filename}`);
+      const file = fs.createReadStream(destination);
+      file.pipe(res);
+    } catch (error: any) {
+      logger.error(error);
+      return res
+        .status(400)
+        .json({ error: { message: error?.message ?? "Server Error" } });
+    }
+  };
+
+  switch (req.method) {
+    case "GET":
+      return await getHandler();
+    default:
+      return res.status(405).send(`${req.method} Not Allowed`);
+  }
+}
