a3833b8f57d837dd54dca826efdc4398fea826e2
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-01-26 10:20:32 +0800

    implemented form f014

---
+++ pages/test/f014/index.tsx
@@ -0,0 +1,159 @@
+import F014 from "@/components/forms/F014";
+import Link from "next/link";
+
+export default function F013Page() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <F014
+        iMTitle="Process of Teaching Physical Education and Health"
+        author="John Bryan Pit M. Acaso"
+        iMType="Module"
+        questions={{
+          q1_1: {
+            contentEditor: "JE",
+            contentSpecialist: "M",
+            iDDSpecialist: "NAA",
+          },
+          q1_2: {
+            contentEditor: "NM",
+            contentSpecialist: "VM",
+            iDDSpecialist: "JE",
+          },
+          q2_1: {
+            contentEditor: "M",
+            contentSpecialist: "NAA",
+            iDDSpecialist: "NM",
+          },
+          q2_2: {
+            contentEditor: "VM",
+            contentSpecialist: "JE",
+            iDDSpecialist: "M",
+          },
+          q2_3: {
+            contentEditor: "NAA",
+            contentSpecialist: "NM",
+            iDDSpecialist: "VM",
+          },
+          q2_4: {
+            contentEditor: "JE",
+            contentSpecialist: "M",
+            iDDSpecialist: "NAA",
+          },
+          q3_1: {
+            contentEditor: "NM",
+            contentSpecialist: "VM",
+            iDDSpecialist: "JE",
+          },
+          q4_1: {
+            contentEditor: "M",
+            contentSpecialist: "NAA",
+            iDDSpecialist: "NM",
+          },
+          q4_2: {
+            contentEditor: "VM",
+            contentSpecialist: "JE",
+            iDDSpecialist: "M",
+          },
+          q4_3: {
+            contentEditor: "NAA",
+            contentSpecialist: "NM",
+            iDDSpecialist: "VM",
+          },
+          q4_4: {
+            contentEditor: "JE",
+            contentSpecialist: "M",
+            iDDSpecialist: "NAA",
+          },
+          q5_1: {
+            contentEditor: "NM",
+            contentSpecialist: "VM",
+            iDDSpecialist: "JE",
+          },
+          q5_2: {
+            contentEditor: "M",
+            contentSpecialist: "NAA",
+            iDDSpecialist: "NM",
+          },
+          q5_3: {
+            contentEditor: "VM",
+            contentSpecialist: "JE",
+            iDDSpecialist: "M",
+          },
+          q5_4: {
+            contentEditor: "NAA",
+            contentSpecialist: "NM",
+            iDDSpecialist: "VM",
+          },
+          q6_1: {
+            contentEditor: "JE",
+            contentSpecialist: "M",
+            iDDSpecialist: "NAA",
+          },
+          q6_2: {
+            contentEditor: "NM",
+            contentSpecialist: "VM",
+            iDDSpecialist: "JE",
+          },
+          q6_3: {
+            contentEditor: "M",
+            contentSpecialist: "NAA",
+            iDDSpecialist: "NM",
+          },
+          q6_4: {
+            contentEditor: "VM",
+            contentSpecialist: "JE",
+            iDDSpecialist: "M",
+          },
+          q6_5: {
+            contentEditor: "NAA",
+            contentSpecialist: "NM",
+            iDDSpecialist: "VM",
+          },
+          q7_1: {
+            contentEditor: "JE",
+            contentSpecialist: "M",
+            iDDSpecialist: "NAA",
+          },
+          q7_2: {
+            contentEditor: "NM",
+            contentSpecialist: "VM",
+            iDDSpecialist: "JE",
+          },
+          q7_3: {
+            contentEditor: "M",
+            contentSpecialist: "NAA",
+            iDDSpecialist: "NM",
+          },
+          q7_4: {
+            contentEditor: "VM",
+            contentSpecialist: "JE",
+            iDDSpecialist: "M",
+          },
+          q7_5: {
+            contentEditor: "NAA",
+            contentSpecialist: "NM",
+            iDDSpecialist: "VM",
+          },
+          q8_1: {
+            contentEditor: "JE",
+            contentSpecialist: "M",
+            iDDSpecialist: "NAA",
+          },
+          q8_2: {
+            contentEditor: "NM",
+            contentSpecialist: "VM",
+            iDDSpecialist: "JE",
+          },
+          q8_3: {
+            contentEditor: "JE",
+            contentSpecialist: "M",
+            iDDSpecialist: "NAA",
+          },
+        }}
+        contentEditorName="Glenmark T. Artiaga"
+        contentSpecialistName="Airys Prill C. Gorra"
+        iDDSpecialistName="Keano Lyrens Jimeno"
+      />
+    </div>
+  );
+}
