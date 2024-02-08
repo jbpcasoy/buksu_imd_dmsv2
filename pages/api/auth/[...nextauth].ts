297d0e4d3cb7e76c1eebf7665d2400c25695886b
jbpcasoy <johnbryanpitcasoy@gmail.com>
2024-02-08 12:55:27 +0800

    reverted prisma siingletom

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient, User } from "@prisma/client";
+import prisma from "@/prisma/client";
 
 if (!process.env.GOOGLE_CLIENT_ID) {
   throw new Error("env GOOGLE_CLIENT_ID is not set");
@@ -10,8 +11,6 @@ if (!process.env.GOOGLE_CLIENT_ID) {
 } else if (!process.env.ADMIN_EMAILS) {
   throw new Error("env ADMIN_EMAILS is not set");
 }
-
-const prisma = new PrismaClient();
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
