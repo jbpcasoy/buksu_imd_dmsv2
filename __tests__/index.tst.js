7be939ba1b3a396b3ba5a8c3ff2c02612f7d4ba3
jbpcasoy <johnbryanpitcasoy@gmal.com>
2024-04-25 11:10:50 +0800

    added jest test

--- __tests__/index.tst.js
+++
@@ -1,7 +0,0 @@
-import "@testing-library/jest-dom";
-
-describe("Add", () => {
-  it("adds 1 + 1 to 2", () => {
-    expect(1 + 1).toBe(2);
-  });
-});
--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,11 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: "v8",
-  testEnvironment: "jsdom",
+  clearMocks: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/prisma/singleton.ts"],
+
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
