72c927476b9d1690c4b3befb29198be667f9cc24
jbpcasoy <72642782+jbpcasoy@users.noreply.github.com>
2024-06-18 10:52:00 +0800

    removed edit suggestion submit icon and improved design

--- pages/im/[id]/coordinator_suggestion/index.tsx
+++ pages/im/[id]/coordinator_suggestion/index.tsx
@@ -550,19 +550,9 @@ function EditSuggestionItem({
               <button
                 disabled={loading}
                 type="submit"
-                className="bg-palette_blue text-white rounded inline-flex items-center justify-center py-1 space-x-2 hover:bg-opacity-90"
+                className="bg-palette_blue text-white inline-flex items-center justify-center py-2 px-4 rounded-md font-semibold text-sm space-x-2 hover:bg-opacity-90"
               >
                 <span>Submit</span>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="1em"
-                    viewBox="0 0 448 512"
-                    className="fill-palette_white"
-                  >
-                    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
-                  </svg>
-                </span>
               </button>
             </div>
           </form>
